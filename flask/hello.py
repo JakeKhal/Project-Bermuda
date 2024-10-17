2a6fa3501c4c1a406dab87a9e406119c11415851
Alexandr Iapara <aiapara@uoregon.edu>
2024-10-17 13:58:58 -0700

    Sample main page on http://127.0.0.1:5000/

--- flask/hello.py
+++ flask/hello.py
@@ -2,28 +2,24 @@ from flask import Flask, render_template, abort
 
 app = Flask(__name__)
 
+@app.route("/")
+def index():
+    return render_template("index.html")
+
 @app.route("/<path:file>")
 def hello(file):
     if "~" in file or "//" in file or ".." in file:
-        abort(403)
+        return render_template('403.html'),403
     elif file.endswith(".html"):
         try:
             return render_template(file),200
         except:
-            abort(404)
+           return render_template('404.html'),404
     else:
-        abort(404)
+        return render_template('404.html'),404
 
 if __name__ == "__main__":
     app.run(debug=True)
-
-@app.errorhandler(404)
-def error_404(error):
-    return render_template('404.html'),404
-
-@app.errorhandler(403)
-def error_403(error):
-    return render_template('403.html'),403
 
 if __name__ == "__main__":
     app.run(debug=True,host='0.0.0.0')
